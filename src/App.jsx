57da35557416fb6f24a55e0786b324f50676761b
Aryaman9999 <aryamansharma.bt22ece@pec.edu.in>
2024-03-25 22:00:59 +0530

    plotting added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import SerialDataReader from '../components/SerialDataReader'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <SerialDataReader/>
+    </>
+  )
+}
+
+export default App
--- src/components/Navbar.tsx
+++
@@ -1,8 +0,0 @@
-function Sidebar() {
-  return (
-    <>
-    </>
-  );
-}
-
-export default Sidebar;
