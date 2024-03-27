4c1224c6a1b53c16a9402a5995b797116c1a44ca
Witty-Wizard <shashankmarch27@gmail.com>
2024-03-27 15:33:06 +0530

    added rocekt orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import "./App.css";
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import GraphComponent from "./components/GrpahComponent";
-import ParentComponent from "./components/ParentComponent";
+import Rocketmodel from "./components/Rocketmodel";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
+  const [orientation, setOrientation] = useState({ x: 0, y: 0, z: 0 });
+
+  // Simulate receiving data
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOrientation({
+        x: Math.random() * 360,
+        y: Math.random() * 360,
+        z: Math.random() * 360,
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className="rocket-container">
+        <Rocketmodel orientation={orientation} />
+      </div>
     </>
   );
 };
