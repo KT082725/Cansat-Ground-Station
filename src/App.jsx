9be656f836937145a52f7bbf7841eb93656d3382
Witty-Wizard <shashankmarch27@gmail.com>
2024-03-29 15:13:03 +0530

    added gagan logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import GraphComponent from "./components/GraphComponent";
-import Rocketmodel from "./components/Rocketmodel";
 import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
-  const data = [
-    { x: new Date('2022-01-01'), y: 10 },
-    { x: new Date('2022-01-02'), y: 20 },
-    { x: new Date('2022-01-03'), y: 30 },
-    { x: new Date('2022-01-04'), y: 25 },
-    { x: new Date('2022-01-05'), y: 35 },
-  ];
 
   return (
     <>
       <Navbar />
-      <div className="graph-container">
-        <GraphComponent data={data} />
-      </div>
     </>
   );
 };
