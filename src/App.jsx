02737472b0b77879c523ac4d5fec1a3bc5ac113d
Aryaman9999 <aryamansharma.bt22ece@pec.edu.in>
2024-04-01 22:48:48 +0530

    Resolved more data issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar"
 import SerialDataReader from "./components/Serial/SerialDataReader";
 const App = () => {
-<<<<<<< HEAD
   const [data, setData] = useState([]);
 
   return (
@@ -11,13 +10,6 @@ const App = () => {
 
       {/* <GraphComponent data={data} /> */}
     </div>
-=======
-
-  return (
-    <>
-      <Navbar />
-    </>
->>>>>>> a79fa3e11619b6d89af5803cc93f0173260d1322
   );
 };
 
