a19ed119624eb3593e2f874e6907d862cb9a8476
Aryaman9999 <aryamansharma.bt22ece@pec.edu.in>
2024-04-02 18:57:43 +0530

    build issues resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import GraphComponent from "./components/GraphComponent";
+import Rocketmodel from "./components/Rocketmodel";
 import Navbar from "./components/Navbar/Navbar"
 import SerialDataReader from "./components/Serial/SerialDataReader";
 const App = () => {
   const [data, setData] = useState([]);
 
+  const handleDataReceived = (newData) => {
+    // Update the data state with the new data
+    setData(prevData => [...prevData, newData]);
+  };
+
   return (
     <div>
-     <SerialDataReader onDataReceived={data => console.log(data)} />
-
+      <SerialDataReader onDataReceived={handleDataReceived} />
       {/* <GraphComponent data={data} /> */}
     </div>
   );
